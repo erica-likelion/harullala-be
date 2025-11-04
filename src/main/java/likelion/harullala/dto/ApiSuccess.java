7c309184d09c14df8c05bffc7f2b21f93b63e6fc
kyoohyunlee <khyhlee@naver.com>
2025-11-05 00:45:44 +0900

    fix: 에러로그 읽게 수정

--- src/main/java/likelion/harullala/dto/ApiSuccess.java
+++ src/main/java/likelion/harullala/dto/ApiSuccess.java
@@ -1,13 +1,15 @@
 package likelion.harullala.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class ApiSuccess<T> {
     private final T data;
     private final String message;
 
-    private ApiSuccess(T data, String message) {
+    public ApiSuccess(T data, String message) {
         this.data = data;
         this.message = message;
     }
